d75f0ea93da8be2b3ea54d8bfb9b868f7a8cc268
santiago <santiagow142001@gmail.com>
2025-04-15 01:55:08 -0300

    Arreglo de Footer

--- nextjs-commerce/components/layout/footer.tsx
+++ nextjs-commerce/components/layout/footer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 
 import FooterMenu from 'components/layout/footer-menu';
-import LogoSquare from 'components/logo-square';
+import { LogoWithText } from 'components/logo-with-text';
 import { Suspense } from 'react';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
@@ -25,12 +25,9 @@ export default async function Footer() {
 
   return (
     <footer className="text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-base md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-800">
-        <div>
-          <Link className="flex items-center gap-2 text-black md:pt-1 dark:text-white" href="/">
-            <LogoSquare size="sm" />
-            <span className="text-base uppercase">{SITE_NAME}</span>
-          </Link>
+      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-800">
+        <div className="flex items-center">
+          <LogoWithText />
         </div>
         <Suspense
           fallback={
